12ad0a7dcd196349130f65d9d40aefc41d419465
JaxV9 <layanj.pro@gmail.com>
2026-08-09 21:35:52 +0200

    refacto

---
+++ src/app/components/chessboard/chessboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chessboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './chessboard.component.html',
+  styleUrl: './chessboard.component.css'
+})
+export class ChessboardComponent {
+
+}
