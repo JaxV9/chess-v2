c3c0b1fc016e76ee8775b71839328ac611ee2836
JaxV9 <layanj.pro@gmail.com>
2026-08-09 21:33:41 +0200

    constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,14 @@
+export enum PieceRole {
+    pawn_black = "pawn_black",
+    pawn_white = "pawn_white",
+    bishop_black = "bishop_black",
+    bishop_white = "bishop_white",
+    king_black = "king_black",
+    king_white = "king_white",
+    queen_black = "queen_black",
+    queen_white = "queen_white",
+    knight_black = "knight_black",
+    knight_white = "knight_white",
+    rook_black = "rook_black",
+    rook_white = "rook_white"
+};
