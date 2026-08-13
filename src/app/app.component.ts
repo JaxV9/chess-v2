9d360948aeae6814b43a5b9e1e4bf614101e80eb
JaxV9 <layanj.pro@gmail.com>
2026-08-13 20:50:13 +0200

    feat: create guest and game session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChessboardComponent } from './components/chessboard/chessboard.component';
+import { ChessFacade } from './store/chess.facade';
+import { HttpService } from './services/http.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, ChessboardComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [ChessboardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'chess-v2';
+  chessFacade = inject(ChessFacade);
+
+  gameSession = toSignal(this.chessFacade.gameSession$);
 }
